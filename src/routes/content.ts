41a15d072ade8a06cdcb39fb71fea3616ce96b44
Dalip Tandon <daliptandon8@gmail.com>
2024-12-01 15:53:03 +0530

    feat:add content,remove content

--- src/routes/content.ts
+++ src/routes/content.ts
@@ -4,10 +4,18 @@ import { userAuthentication } from "../middlewares/userAuth";
 const contentRouter=express.Router();
 
 
-contentRouter.post("/v1/content" ,userAuthentication,(req:Request,res:Response)=>{
+contentRouter.post("/v1/content" ,userAuthentication,async(req:Request,res:Response)=>{
     try{
-        res.send("endpoint hit")
+      const{type,link,title,tags}=req.body;
+      const userId=req.user._id;
+      const content=await contentModel.create({
+        type,link,tags,title,
+        userId
+      });
 
+      res.status(200).json({
+        message:"Content successfully added"
+      })
 
     }catch(error:any){
         res.status(400).json({
@@ -16,6 +24,28 @@ contentRouter.post("/v1/content" ,userAuthentication,(req:Request,res:Response)=
     }
 })
 
+contentRouter.delete("/v1/content/:contentId",userAuthentication,async(req:Request,res:Response)=>{
+    try{
+        const contentId=req.params.contentId;
+        const userId=req.user._id;
+
+        const data=await contentModel.deleteOne({
+            _id:contentId,
+            userId:userId
+        });
+        if(data.deletedCount ===0){
+            throw new Error("Content not found");
+        }
+        res.status(200).json({
+            message:"Your content is deleted"
+        })
+
+    }catch(error:any){
+        res.status(400).json({
+            message:error.message || "Internal server error"
+        })
+    }
+})
 
 
 
